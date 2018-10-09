0db2050a16b78a1352bfa57906cd86accc79ab89
Jannik Kramer <kramer@consider-it.de>
2018-10-09 14:59:44 +0200

    Docker Hub test

---
+++ main.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"github.com/urfave/cli"
+	"os"
+)
+
+// build number set at compile-time
+var build = "0"
+
+// Version set at compile-time
+var Version string
+
+var plugin = Plugin{
+	Writer: os.Stdout,
+}
+
+func main() {
+	if Version == "" {
+		Version = fmt.Sprintf("0.1.0+%s", build)
+	}
+
+	// Load env-file if it exists first
+	if filename, found := os.LookupEnv("PLUGIN_ENV_FILE"); found {
+		_ = godotenv.Load(filename)
+	}
+
+	app := cli.NewApp()
+	app.Name = "Drone DigitalOcean"
+	app.Usage = "Deploy your application easly to DigitalOcean"
+	app.Copyright = "Copyright (c) Jannik Kramer"
+	app.Authors = []cli.Author{
+		{
+			Name:  "Jannik Kramer",
+			Email: "mail@jannikkramer.de",
+		},
+	}
+	app.Action = run
+	app.Version = Version
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "loadbalancer,b",
+			Usage:  "loadbalancer to deploy to",
+			EnvVar: "PLUGIN_LOADBALANCER",
+			Destination: &plugin.Config.Loadbalancer,
+		},
+		cli.StringFlag{
+			Name: "tag",
+			Usage: "tag to deploy to",
+			EnvVar: "PLUGIN_TAG",
+			Destination: &plugin.Config.Tag,
+		},
+		cli.StringFlag{
+			Name:   "user,u",
+			Usage:  "connect as user",
+			EnvVar: "PLUGIN_SSH_USER",
+			Destination: &plugin.Config.User,
+		},
+		cli.StringFlag{
+			Name:   "key-path,i",
+			Usage:  "ssh private key path",
+			EnvVar: "PLUGIN_SSH_KEY_PATH",
+			Destination: &plugin.Config.KeyPath,
+		},
+		cli.StringFlag{
+			Name:   "key",
+			Usage:  "ssh private key",
+			EnvVar: "PLUGIN_SSH_KEY",
+			Destination: &plugin.Config.Key,
+		},
+		cli.StringFlag{
+			Name:   "password",
+			Usage:  "ssh password",
+			EnvVar: "PLUGIN_SSH_PASSWORD",
+			Destination: &plugin.Config.Password,
+		},
+		cli.IntFlag{
+			Name:   "port,p",
+			Usage:  "ssh port",
+			EnvVar: "PLUGIN_SSH_PORT",
+			Destination: &plugin.Config.Port,
+		},
+		cli.DurationFlag{
+			Name:   "timeout",
+			Usage:  "ssh timeout",
+			EnvVar: "PLUGIN_SSH_TIMEOUT",
+			Destination: &plugin.Config.Timeout,
+		},
+		cli.StringFlag{
+			Name:   "pat,t",
+			Usage:  "digitalocean personal access token",
+			EnvVar: "PLUGIN_PAT",
+			Destination: &plugin.Config.Pat,
+		},
+		cli.StringFlag{
+			Name:   "source",
+			Usage:  "folder to copy from",
+			EnvVar: "PLUGIN_SOURCE",
+			Destination: &plugin.Config.SourcePath,
+		},
+		cli.StringFlag{
+			Name:   "target",
+			Usage:  "folder to copy to",
+			EnvVar: "PLUGIN_TARGET",
+			Destination: &plugin.Config.TargetPath,
+		},
+		cli.StringFlag{
+			Name:   "pre-sync",
+			Usage:  "script to run before sync",
+			EnvVar: "PLUGIN_PRE_SYNC",
+			Destination: &plugin.Config.PreSync,
+		},
+		cli.StringFlag{
+			Name:   "post-sync",
+			Usage:  "script to run after sync",
+			EnvVar: "PLUGIN_POST_SYNC",
+			Destination: &plugin.Config.PostSync,
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+}
+
+// this runs due to "app.Action = run" in main()
+func run(c *cli.Context) error {
+	plugin.Config.KeyPath = "id_sha"
+
+	if err := plugin.Exec(); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ plugin.go
@@ -0,0 +1,237 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/digitalocean/godo"
+	"golang.org/x/oauth2"
+	"io"
+	"time"
+)
+
+type (
+	Config struct {
+		Loadbalancer string
+		Tag          string
+		User         string
+		KeyPath      string
+		Key          string
+		Password     string
+		Port         int
+		Timeout      time.Duration
+		Pat          string
+		SourcePath   string
+		TargetPath   string
+		PreSync      string
+		PostSync     string
+	}
+
+	Plugin struct {
+		Config Config
+		Hosts  []Host
+		Writer io.Writer
+	}
+
+	Host struct {
+		ID int
+		IP string
+	}
+
+	TokenSource struct {
+		AccessToken string
+	}
+)
+
+const (
+	onlyOneHost          = "please provide only a loadbalancer or a tag"
+	noHost               = "please provide a loadbalancer or a tag"
+	tagNotFound          = "provided tag not found or no droplets assigned"
+	loadbalancerNotFound = "provided loadbalancer not found"
+	onlyOneCredential    = "please provide a key or a password"
+	noCredentials        = "no key or password provided"
+	noPAT                = "no PAT provided"
+	noDropletsFound      = "no droplets found"
+	couldntResolveIPs    = "could not resolve IDs to IPs"
+)
+
+func (t *TokenSource) Token() (*oauth2.Token, error) {
+	token := &oauth2.Token{
+		AccessToken: t.AccessToken,
+	}
+	return token, nil
+}
+
+// Validate checks the values of the config parameters.
+// If the examined parameter did not pass the check, either a default value
+// is set or, if this is not possible or useful, an error is returned.
+func (c *Config) Validate() error {
+	// asset both set
+	if len(c.Loadbalancer) > 0 && len(c.Tag) > 0 {
+		return fmt.Errorf(onlyOneHost)
+	}
+
+	// assert none set
+	if len(c.Loadbalancer) == 0 && len(c.Tag) == 0 {
+		return fmt.Errorf(noHost)
+	}
+
+	// asset user unset
+	if len(c.User) == 0 {
+		c.User = "root"
+	}
+
+	// assert more than one set
+	if (len(c.KeyPath) > 0 || len(c.Key) > 0) && len(c.Password) > 0 {
+		return fmt.Errorf(onlyOneCredential)
+	}
+
+	// assert none set
+	if len(c.KeyPath) == 0 && len(c.Key) == 0 && len(c.Password) == 0 {
+		return fmt.Errorf(noCredentials)
+	}
+
+	// assert port unset
+	if c.Port <= 0 {
+		c.Port = 22
+	}
+
+	// assert timeout unset
+	if c.Timeout == time.Duration(0) {
+		c.Timeout = time.Duration(60)
+	}
+
+	// assert token unset
+	if len(c.Pat) == 0 {
+		return fmt.Errorf(noPAT)
+	}
+
+	// assert source unset
+	if len(c.SourcePath) == 0 {
+		c.SourcePath = "./"
+	}
+
+	// assert target unset
+	if len(c.TargetPath) == 0 {
+		c.TargetPath = "./"
+	}
+
+	return nil
+}
+
+func (p Plugin) getClient() *godo.Client {
+	tokenSource := &TokenSource{
+		AccessToken: p.Config.Pat,
+	}
+
+	oauthClient := oauth2.NewClient(context.TODO(), tokenSource)
+	return godo.NewClient(oauthClient)
+}
+
+// getDropletIDsByLoadbalancer searches for a loadbalancer with the
+// specified name and, if found, returns its droplets as Hosts.
+func (p Plugin) getHostsByLoadbalancer() ([]Host, error) {
+	client := p.getClient()
+
+	// get all loadbalancer
+	lbs, _, err := client.LoadBalancers.List(context.TODO(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// get all droplets
+	droplets, _, err := client.Droplets.List(context.TODO(), nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// intersect loadbalancer
+	var ids []int
+	for _, lb := range lbs {
+		if lb.Name == p.Config.Loadbalancer {
+			ids = lb.DropletIDs
+		}
+	}
+
+	// intersect loadbalancer and droplets
+	var hosts []Host
+	for _, droplet := range droplets {
+		for _, id := range ids {
+			if droplet.ID == id {
+				if ip, err := droplet.PublicIPv4(); err == nil {
+					hosts = append(hosts, Host{ID: id, IP: ip})
+				}
+			}
+		}
+	}
+
+	if len(hosts) == 0 {
+		return nil, fmt.Errorf(noDropletsFound)
+	}
+
+	return hosts, nil
+}
+
+// getHostsByTag searches for a tag with the specified name and, if
+// found, returns its droplets as Hosts.
+func (p Plugin) getHostsByTag() ([]Host, error) {
+	client := p.getClient()
+
+	// get droplets with tag
+	droplets, _, err := client.Droplets.ListByTag(context.TODO(), p.Config.Tag, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	// covert to Droplet to Host
+	var hosts []Host
+	for _, droplet := range droplets {
+		if ip, err := droplet.PublicIPv4(); err == nil {
+			hosts = append(hosts, Host{ID: droplet.ID, IP: ip})
+		}
+	}
+
+	if len(hosts) == 0 {
+		return nil, fmt.Errorf(noDropletsFound)
+	}
+
+	return hosts, nil
+}
+
+func (p Plugin) runPreSyncScript() error {
+
+
+	return nil
+}
+
+func (p *Plugin) Exec() error {
+	// check command line arguments
+	if err := p.Config.Validate(); err != nil {
+		return err
+	}
+
+	if len(p.Config.Loadbalancer) != 0 {
+		hosts, err := p.getHostsByLoadbalancer()
+		if err != nil {
+			return err
+		}
+		p.Hosts = hosts
+	}
+
+	if len(p.Config.Tag) != 0 {
+		hosts, err := p.getHostsByTag()
+		if err != nil {
+			return err
+		}
+		p.Hosts = hosts
+	}
+
+	fmt.Println(p.Hosts)
+
+	// TODO: pre sync script
+
+	// TODO: sync
+
+	// TODO: post sync script
+
+	return nil
+}
